6aaf812e0162ec05ef41f437f7d865f32c141c61
Adam Klein <adam3207@gmail.com>
2023-02-28 16:16:39 -0800

    fixed metric calculation

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -143,6 +143,7 @@ def train(model, optimizer, train_data, loss_fn, val_data=None, num_epochs=None,
                 postfix = {'epoch': epoch, 'step': step, 'mem_use': mem_use, 'loss': rolling_tot_loss / rollong_loss_num}
 
                 if metric is not None:
+                    rolling_metric *= rolling_avg
                     rolling_metric += metric(train_preds[-1], train_y[-1])
                     postfix[metric.title] = rolling_metric / rollong_loss_num
 
