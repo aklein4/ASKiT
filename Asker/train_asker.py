9c56a0afb4cceaa79aca56007af47423a0e3dbd0
mattville <mattjv22@stanford.edu>
2023-03-15 20:40:07 -0700

    Adam I shrunk the kids

--- Asker/train_asker.py
+++ Asker/train_asker.py
@@ -24,10 +24,6 @@ MAX_INPUT_LENGTH = 512
 
 MAX_TARGET_LENGTH = 128
 
-def appendGenPrefix(data):
-    for d in data:
-        d["chosen"] = "generate question: " + d["chosen"].strip()
-
 
 def removeSepToken(data):
     for d in data:
@@ -35,55 +31,61 @@ def removeSepToken(data):
 
 
 def main():
-    ds = load_dataset("json", data_files=DATA_PATH)
-    print(ds['train'][0])
-    return
-    with open(DATA_PATH) as f:
-        data = json.load(f)
+    data = load_dataset("json", data_files=DATA_PATH)
+    
+    # Load tokenizer/asker model
+    asker = T5ForConditionalGeneration.from_pretrained(ASKER_MODEL)
+    tokenizer = T5TokenizerFast.from_pretrained("t5-base", model_max_length=512)
+    
+    # Consider '<sep>' token
+    tokenizer.sep_token = '<sep>'
+    tokenizer.add_tokens(['<sep>'])
+    asker.resize_token_embeddings(len(tokenizer))
+    
+    # Define some in-line processing functions
+    def addGenPrefix(example):
+        example['chosen'] = "generate question: " + example['chosen'].strip()
+        return example
 
-        # Process data
-        appendGenPrefix(data)
+    def convertToFeatures(example_batch):
 
-        # Load tokenizer/asker model
-        asker = T5ForConditionalGeneration.from_pretrained(ASKER_MODEL)
-        tokenizer = T5TokenizerFast.from_pretrained("t5-base", model_max_length=512)
-        
-        # Consider '<sep>' token
-        tokenizer.sep_token = '<sep>'
-        tokenizer.add_tokens(['<sep>'])
-        asker.resize_token_embeddings(len(tokenizer))
-        
-        # Define some in-line processing functions
-        def convert_to_features(example_batch):
+        input_encodings = tokenizer.batch_encode_plus(example_batch['context'], 
+                                                        max_length=MAX_INPUT_LENGTH, 
+                                                        add_special_tokens=True,
+                                                        truncation=True, 
+                                                        pad_to_max_length=True)
 
-            input_encodings = tokenizer.batch_encode_plus(example_batch['context'], 
-                                                            max_length=MAX_INPUT_LENGTH, 
-                                                            add_special_tokens=True,
-                                                            truncation=True, 
-                                                            pad_to_max_length=True)
+        target_encodings = tokenizer.batch_encode_plus(example_batch['questions'], 
+                                                        max_length=MAX_TARGET_LENGTH, 
+                                                        add_special_tokens=True,
+                                                        truncation=True, pad_to_max_length=True)
+                                                        
+        encodings = {
+            'input_ids': input_encodings['input_ids'], 
+            'attention_mask': input_encodings['attention_mask'],
+            'decoder_input_ids': target_encodings['input_ids']
+            ,'decoder_attention_mask': target_encodings['attention_mask']
+        }
 
-            target_encodings = tokenizer.batch_encode_plus(example_batch['questions'], 
-                                                            max_length=MAX_TARGET_LENGTH, 
-                                                            add_special_tokens=True,
-                                                            truncation=True, pad_to_max_length=True)
-                                                            
-            encodings = {
-                'input_ids': input_encodings['input_ids'], 
-                'attention_mask': input_encodings['attention_mask'],
-                'decoder_input_ids': target_encodings['input_ids']
-                ,'decoder_attention_mask': target_encodings['attention_mask']
-            }
+        return encodings
 
-            return encodings
+    def addEOS(example):
+        example['context'] = example['context'] + " </s>"
+        example['questions'] = example['questions'] + " </s>"
+        return example
+    
+    def removeSepTokens(example):
+        example['questions'] = example['questions'].replace('<sep>', '')
+        return example
 
-        def add_eos_examples(example):
-            example['context'] = example['context'] + " </s>"
-            example['questions'] = example['questions'] + " </s>"
-            return example
-
-        #tok_data  = map(add_eos_examples, data)
-        #tok_data = tok_data.map(convert_to_features, batched=True)
-        #print(tok_data[0]["question"])
+    # Process data
+    tok_data = data.map(addGenPrefix)
+    tok_data = tok_data.map(addEOS)
+    tok_data = tok_data.map(convertToFeatures, batched=True)
+    print(tok_data["train"][0]["chosen"])
+    #tok_data  = map(add_eos_examples, data)
+    #tok_data = tok_data.map(convert_to_features, batched=True)
+    #print(tok_data[0]["question"])
 
     #print("Loading data...")
     #data = load_dataset("json", DATA_PATH)
