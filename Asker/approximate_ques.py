17290ce7bdf25782983bdf99fb31b03155e36e00
mattville <mattjv22@stanford.edu>
2023-03-18 22:05:34 -0700

    for results

--- Asker/approximate_ques.py
+++ Asker/approximate_ques.py
@@ -64,21 +64,21 @@ def main():
         INPUT_ENCODINGS,
         search, chooser,
         8, device=torch.device(DEVICE),
-        data_end=10
+        data_end=30
     )
 
     for i in range(len(env.data)):
         question = env.data[i]['question']
         chosen = env.greedyRollout(i, "", env.data[i]["raw_corpus"], env.corpus[i].float())
-        amalg = '<sep>'.join(chosen[1:])
+        amalg = '<sep>'.join(chosen)
         input_string = "generate question: " + amalg + " </s>"
         input_ids = example_tokenizer.encode(input_string, return_tensors="pt", truncation=True)
         res = example_asker.generate(input_ids, **GENERATOR_ARGS)
         output = example_tokenizer.batch_decode(res, skip_special_tokens=True)
         #output = [item.split("<sep>") for item in output][0][0].split("?")[0]+"?"        
         print("\n-----------------")
-        print('Original Question\n', question, chosen[0])
-        print('Chosen Evidence\n', chosen[1:])
+        print('Original Question\n', question)
+        print('Chosen Evidence\n', chosen)
         # print('Target Evidence\n', target_evidence)
         print('New Question\n', output)
 
